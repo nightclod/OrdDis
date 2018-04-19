dfe2c1a6585d83ef17b7545c77cac3dc658995b0
qiqiang <qiq@shinemo.com>
2018-04-19 19:31:56 +0800

    修改bug

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -203,7 +203,8 @@ export default {
     name: 'home',
     data () {
         return {
-            scrollWatch: true
+            scrollWatch: true,
+            idApp: document.getElementById("app")
         }
     },
     mounted () {
@@ -214,12 +215,12 @@ export default {
             if( !Base.gitCookie("ordDisCooike") ){
                 this.$router.push({path:'/login'});
             }
-            document.getElementById("app").scrollTop = 0;
-            document.getElementById("app").addEventListener('scroll', this.handleScroll);
+            this.idApp.scrollTop = 0;
+            this.idApp.addEventListener('scroll', this.handleScroll);
         },
         handleScroll () {
             if(this.scrollWatch){
-                var scrollTop = document.getElementById("app").scrollTop;
+                var scrollTop = this.idApp.scrollTop;
                 if(scrollTop >= 80 ){
                     this.$refs.top.style.height = "3rem"
                 }else{
